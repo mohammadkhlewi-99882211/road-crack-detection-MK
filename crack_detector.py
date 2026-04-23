62eebf550b3ce5e11207a21d42ea717b725dc412
mohammadkhlewi-99882211 <mohammad.k5247019@gmail.com>
2026-04-23 12:09:26 +0300

    Update crack_detector.py

--- crack_detector.py
+++ crack_detector.py
@@ -179,7 +179,7 @@ def draw_ai_detections(image_np, cracks):
             continue
 
         # ← تحسين الـ bbox بمعالجة الصورة
-        bbox = refine_bbox(image_np, raw_bbox, expand=0.25)
+        bbox = raw_bbox
 
         nx = float(bbox["x"]);     ny = float(bbox["y"])
         nw = float(bbox["width"]); nh = float(bbox["height"])
